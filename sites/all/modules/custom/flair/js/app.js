fdb83b36dfffef497850f96f694345cc3a53c87f
Joe Crespo <joe@atendesigngroup.com>
2013-11-05 19:48:43 -0700

    Boomgit add .

--- sites/all/modules/custom/flair/js/app.js
+++ sites/all/modules/custom/flair/js/app.js
@@ -1,9 +1,47 @@
 
 (function($) {
 
+function add_flair() {
+  var flairs = Drupal.settings.flair;
+  winW = $(window).width() - 100;
+  winH = $(window).height() - 100;
+  $('.flair').remove();
+
+  for(i=0;i<flairs.length;i++) {
+    
+    $div = $('<div />');
+    $div.addClass('flair');
+    $div.html($('<a />').attr('href', flairs[i].url).html(flairs[i].title));
+    w = Math.random() * (winW - 10) + 0;
+    h = Math.random() * (winH + $(document).scrollTop() - 10) + 0;
+    $div.css('left', w);    
+    $div.css('top', h);    
+    
+    console.log(flairs[i]);    
+    $div.css('background-image', 'url(' + flairs[i].url + ')');
+    
+    $('body').prepend($div);
+
+  }    
+  $('.flair').draggable();
+}
+
+
 Drupal.behaviors.flair = {
   attach: function() {
-    console.log(Drupal.settings.flair);    
+
+    $(window).resize(function() {
+      add_flair();
+    });
+
+    $(window).scroll(function() {
+      add_flair();
+    });
+
+    $(document).ready(function() {
+      add_flair();
+    });
+    
   }  
 };
 
